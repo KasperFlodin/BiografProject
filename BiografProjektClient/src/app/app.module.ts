ce3a5e7ea4404f2d12355a44aeb64aa6dbc86595
Kasper Flodin <kasperflodin@gmail.com>
2023-11-13 09:25:14 +0100

    BiografClient components

--- BiografProjektClient/src/app/app.module.ts
+++ BiografProjektClient/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { HallComponent } from './components/hall/hall.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { UserComponent } from './components/user/user.component';
+import { GenreComponent } from './components/genre/genre.component';
+import { SeatComponent } from './components/seat/seat.component';
+
+// ng add @ng-bootstrap/ng-bootstrap
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    HallComponent,
+    TicketComponent,
+    UserComponent,
+    GenreComponent,
+    SeatComponent
   ],
   imports: [
     BrowserModule,

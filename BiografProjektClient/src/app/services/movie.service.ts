2246fe8ea5e3fea2f3dbb199b92e77b0f5636957
Kasper <kasperflodin@gmail.com>
2023-11-20 15:35:48 +0100

    service

--- BiografProjektClient/src/app/services/movie.service.ts
+++ BiografProjektClient/src/app/services/movie.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../Environment/Environment';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,24 @@ export class MovieService {
   constructor(private http:HttpClient) { }
 
 
-
-
-
-
   getAll():Observable<Movie[]>{
-    return this.http.get<Hero[]>(this.apiUrl)
+    return this.http.get<Movie[]>(this.apiUrl)
+  }
+
+  delete(movieId: number): Observable<Movie> {
+    return this.http.delete<Movie>(this.apiUrl+movieId)
+  }
+
+  create(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.apiUrl, movie);
+  }
+
+  update(movieId:number, movie: Movie): Observable<Movie> {
+    return this.http.put<Movie>(this.apiUrl+movieId, movie);
+  }
+
+  FindById(movieId: string): Observable<Movie> {
+    return this.http.get<Movie>(this.apiUrl+movieId);
   }
 }
 
